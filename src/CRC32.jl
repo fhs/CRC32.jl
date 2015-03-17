f034d66c090a890731f6ec68dfad9d884ebc7512
Fazlul Shahriar <fshahriar@gmail.com>
2015-03-17 01:35:56 -0400

    fix build on julia v0.3

--- src/CRC32.jl
+++ src/CRC32.jl
@@ -2,10 +2,12 @@ module CRC32
 
 export crc32
 
+using Compat
+
 function maketable(poly::UInt32)
 	tab = zeros(UInt32, 256)
 	for i in 0:255
-		crc = UInt32(i)
+		crc = @compat UInt32(i)
 		for _ in 1:8
 			if (crc&1) == 1
 				crc = (crc >> 1) $ poly
@@ -21,9 +23,9 @@ end
 const table = maketable(0xedb88320)
 
 function crc32(data::Vector{UInt8}, crc::Integer=0)
-	crc = ~UInt32(crc)
+	crc = ~@compat UInt32(crc)
 	for b in data
-		crc = table[(UInt8(crc&0xff) $ b) + 1] $ (crc >> 8)
+		crc = table[(@compat UInt8(crc&0xff) $ b) + 1] $ (crc >> 8)
 	end
 	~crc
 end
