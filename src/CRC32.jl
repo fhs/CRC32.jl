e5566332ad70f60fce55d543ff4b722ee9431725
Steven G. Johnson <stevenj@alum.mit.edu>
2023-01-11 13:43:47 -0500

    julia 1.x compatibility

--- src/CRC32.jl
+++ src/CRC32.jl
@@ -2,15 +2,13 @@ module CRC32
 
 export crc32
 
-using Compat
-
 function maketable(poly::UInt32)
 	tab = zeros(UInt32, 256)
 	for i in 0:255
-		crc = @compat UInt32(i)
+		crc = UInt32(i)
 		for _ in 1:8
-			if (crc&1) == 1
-				crc = (crc >> 1) $ poly
+			if (crc & 1) == 1
+				crc = (crc >> 1) ⊻ poly
 			else
 				crc >>= 1
 			end
@@ -22,14 +20,14 @@ end
 
 const table = maketable(0xedb88320)
 
-function crc32(data::Vector{UInt8}, crc::Integer=0)
-	crc = ~@compat UInt32(crc)
+function crc32(data::AbstractVector{UInt8}, crc_::Integer=0)
+	crc = ~UInt32(crc_)
 	for b in data
-		crc = table[(@compat UInt8(crc&0xff) $ b) + 1] $ (crc >> 8)
+		crc = table[(UInt8(crc&0xff) ⊻ b) + 1] ⊻ (crc >> 8)
 	end
 	~crc
 end
 
-crc32(data::String, crc::Integer=0) = crc32(convert(Vector{UInt8}, data), crc)
+crc32(data::AbstractString, crc::Integer=0) = crc32(codeunits(data), crc)
 
 end # module
