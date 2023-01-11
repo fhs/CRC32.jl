5d4bddf2ce819f5f13acd0f6b478ff44ada3e2d9
Steven G. Johnson <stevenj@alum.mit.edu>
2023-01-11 14:12:50 -0500

    tabs to spaces

--- src/CRC32.jl
+++ src/CRC32.jl
@@ -3,29 +3,29 @@ module CRC32
 export crc32
 
 function maketable(poly::UInt32)
-	tab = zeros(UInt32, 256)
-	for i in 0:255
-		crc = UInt32(i)
-		for _ in 1:8
-			if (crc & 1) == 1
-				crc = (crc >> 1) ⊻ poly
-			else
-				crc >>= 1
-			end
-		end
-		tab[i+1] = crc
-	end
-	tab
+    tab = zeros(UInt32, 256)
+    for i in 0:255
+        crc = UInt32(i)
+        for _ in 1:8
+            if (crc & 1) == 1
+                crc = (crc >> 1) ⊻ poly
+            else
+                crc >>= 1
+            end
+        end
+        tab[i+1] = crc
+    end
+    tab
 end
 
 const table = maketable(0xedb88320)
 
 function crc32(data::AbstractVector{UInt8}, crc::UInt32=UInt32(0))
-	crc = ~crc
-	@inbounds for b in data
-		crc = table[((crc % UInt8) ⊻ b) + 1] ⊻ (crc >> 8)
-	end
-	~crc
+    crc = ~crc
+    @inbounds for b in data
+        crc = table[((crc % UInt8) ⊻ b) + 1] ⊻ (crc >> 8)
+    end
+    ~crc
 end
 
 crc32(data, crc::Integer) = crc32(data, UInt32(crc))
